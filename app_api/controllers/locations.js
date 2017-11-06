8f74565cbcd35ce471156cea25a8f5dccd6cd4f1
longueiraemiliano <emiliano.longueira@cencosud.com.ar>
2017-11-06 17:37:10 -0300

    ch06

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+var sendJsonResponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+module.exports.locationsCreate = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
+
+module.exports.locationsReadOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
